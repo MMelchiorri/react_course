77f5f6b6ccb2f248ea720c4b1c2292df581ce964
Marco Melchiorri <marco.melchiorri@verisure.it>
2024-06-13 10:48:07 +0200

    completed lesson 25

--- code_evolution_react/src/App.js
+++ code_evolution_react/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import LifecycleA from './components/LifecycleA';
+import Fragment from './components/Fragment';
+import Table from './components/Table';
 function App() {
   return (
     <div className="App">
-      <LifecycleA />
+      {/*<Fragment />*/}
+      <Table />
     </div>
   );
 }
---
+++ code_evolution_react/src/components/Fragment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Fragment() {
+  return (
+    <React.Fragment>
+      <h1>This is a fragment demo</h1>
+      <p>this is a paragraph of fragment demo</p>
+    </React.Fragment>
+  );
+}
+
+export default Fragment;
